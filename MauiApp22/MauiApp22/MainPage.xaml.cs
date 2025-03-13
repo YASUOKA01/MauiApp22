b56816b2d78baf44c6438f2f7b5aa6030de0ef79
YASUOKA01 <Eduardoyasuoka81@gmail.com>
2025-03-13 16:35:28 -0300

    Adicionar arquivos de projeto.

---
+++ MauiApp22/MauiApp22/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿namespace MauiApp22
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private void btnProduto_Clicked(object sender, EventArgs e)
+        {
+            Produto produto = new Produto();
+
+            produto.Nome = "Buzina";
+            produto.Preco = 300;
+            produto.Categoria = "Acessórios";
+
+            Navigation.PushAsync(new NewPage1() { BindingContext = produto });
+        }
+
+
+        private void btnCama_Clicked(object sender, EventArgs e)
+        {
+            Produto produto = new Produto();
+
+            produto.Nome = "Cama";
+            produto.Preco = 3000;
+            produto.Categoria = "Casa";
+
+            Navigation.PushAsync(new NewPage1() { BindingContext = produto });
+
+        }
+
+        private void btnLista_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ListaProdutoPage());
+            {
+
+            }
+        }
+    }
+
+}
