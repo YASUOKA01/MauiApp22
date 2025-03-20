bf5b182d277576f22e78f20433e4a2d8e91351ff
YASUOKA01 <Eduardoyasuoka81@gmail.com>
2025-03-20 16:24:07 -0300

    projeto 123

--- MauiApp22/MauiApp22/ListaProdutoPage.xaml.cs
+++ MauiApp22/MauiApp22/ListaProdutoPage.xaml.cs
@@ -4,6 +4,18 @@ public partial class NewPage2 : ContentPage
 {
 	public NewPage2()
 	{
+		List<Produto> lista = Produto.Lista;
+
 		InitializeComponent();
+
+        lstProduto.ItemsSource = lista;
+
+    }
+
+    private void ViewCell_Tapped(object sender, EventArgs e)
+	{
+		Navigation.PushAsync(new ProdutoPage() { 
+			BindingContext = ((ViewCell)sender).BindingContext });
 	}
+
 }
--- MauiApp22/MauiApp22/MainPage.xaml.cs
+++ MauiApp22/MauiApp22/MainPage.xaml.cs
@@ -54,4 +54,7 @@
         }
     }
 
+    internal class ListaProdutoPage : Page
+    {
+    }
 }
